4fe6cf369ad2b2357e166f1372cb45ccd7a2b1aa
unknown <chirazgouissem@gmail.com>
2023-11-16 22:22:56 +0100

    com1

---
+++ lab4.py
@@ -0,0 +1,8 @@
+# Programme Python
+noms = ["Shiraz", "Rabiaa", "Othmen", "Hassen"]
+
+# Concaténation des noms avec l'espace " "
+message = "Bonjour " + " ".join(noms)
+
+# Affichage du message
+print(message)
